218915730364c553e8ad8888b044aa51036b5486
Justin Page <xjustinpagex@gmail.com>
2014-04-24 17:02:45 -0700

    default mailer and Mailer indentation

--- app/mailers/Mailer.php
+++ app/mailers/Mailer.php
@@ -3,13 +3,13 @@
 use Mail;
 
 abstract class Mailer {
-	public function sendTo($user, $subject, $view, $data = []) 
+	public function sendTo($user, $subject, $view, $data = [])
 	{
 		Mail::queue($view, $data, function($message) use($user, $subject)
-		{
-			$message->to($user->email)
-				->subject($subject);
-		});
+	{
+		$message->to($user->email)
+			->subject($subject);
+	});
 
 	}
 }
