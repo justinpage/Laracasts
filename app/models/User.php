8b813ce785d494044a62d4e5df06dc2cf15408ed
Justin Page <xjustinpagex@gmail.com>
2014-04-26 12:19:53 -0700

    config db, fixed session hijacking

--- app/models/User.php
+++ app/models/User.php
@@ -69,4 +69,19 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 
 		return false;
 	}
+
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+		$this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
 }
