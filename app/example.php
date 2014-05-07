b1b9a31c2c95b713c9ae8d00860eebaf0420ccfe
Justin Page <xjustinpagex@gmail.com>
2014-05-06 23:00:44 -0700

    Example for using IteratorAggregate and Countable classes

--- app/example.php
+++ app/example.php
@@ -1,15 +1,36 @@
 <?php
 
-class Library {
+class Library implements IteratorAggregate, Countable {
+	protected $books;
+
 	function __construct(array $books)
 	{
 		$this->books = $books;
 	}
 
+	public function getIterator()
+	{
+		return new ArrayIterator($this->books);
+	}
+
+	public function count()
+	{
+		return count($this->books);
+	}
 }
 
 class Book {
+	protected $title;
 
+	function __construct($title)
+	{
+		$this->title = $title;
+	}
+
+	function getBookTitle()
+	{
+		return $this->title;
+	}
 }
 
 $books[] = new Book('Book one');
@@ -19,5 +40,7 @@ $books[] = new Book('Book three');
 $lib = new Library($books);
 
 foreach ($lib as $book) {
-	var_dump($book);
+	echo $book->getBookTitle() . PHP_EOL;
 }
+
+echo "Total Books: " . $lib->count() . PHP_EOL;
