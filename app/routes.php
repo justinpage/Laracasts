59b390214e80d62490a0ad7551ae2537a3362f25
Justin Page <xjustinpagex@gmail.com>
2014-05-24 22:40:02 -0700

    init for decoding facades --not much edited

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,9 @@
 <?php
 
-Route::get('/', function()
-{
-	return 'hello!';
-});
+// Facades give us a pretty static interface for interacting Laravel
+// going past dependencies, instantiation, and any other conflicts within the IoC container
+// support classes that allow us to access an object in the IoC container
 
+// {{ Form::open }}
+//
+// {{ Form::close() }}
