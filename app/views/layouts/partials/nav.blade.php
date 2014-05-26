37817bfe722e953b781bcf1298efa191926fa158
Justin Page <xjustinpagex@gmail.com>
2014-05-26 15:10:42 -0700

    fixed Home link 404 exception

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <header>
 	<h2>{{ link_to_route('orders.index', 'Demos!') }}</h2>
 	<ul>
-		<li>{{ link_to('order.index', 'Home') }}</li>
+		<li>{{ link_to_route('orders.index', 'Home') }}</li>
 	</ul>
 </header>
