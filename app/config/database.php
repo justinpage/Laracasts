ff268246ab4e9d65068b0f1d871a6a1007178db0
Justin Page <xjustinpagex@gmail.com>
2014-08-07 17:35:11 -0700

    update: production mysql and debug on

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'laracasts',
+			'username'  => 'root',
+			'password'  => 'klvtz',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
